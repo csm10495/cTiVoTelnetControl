a1e035ca6d292619db45b0bd7bd502c1ffb0ed02
csm10495 <csm10495@gmail.com>
2015-05-31 17:58:39 -0400

    added config, added some error checking. Attempt to reconnect if connection is lost

--- cTivoTelnetControl/cTivoTelnetControl/cTivoTelnetControl.py
+++ cTivoTelnetControl/cTivoTelnetControl/cTivoTelnetControl.py
@@ -8,6 +8,7 @@ import telnetlib  #for telnet connection
 import os         #for os.linesep (\n vs \r\n)
 import sys        #for args
 import getch      #local file for getch in Windows and Unix
+import socket     #for socket.error
 
 
 #makes connection to the given TiVo ip, port on default TiVo port
@@ -23,17 +24,24 @@ def getKeyToTelnet():
     with open('RemoteToKeyMappings.txt', 'r') as file:
         for line in file: 
             vals = line.split('\t')
-            #vals[0] is the remote button, not used by code, makes
-            #the file more human readable
+            #vals[0] is the remote button, not used by code
+            #makes the file more human readable
             d[vals[1]] = vals[2].rstrip()
     return d
 
 #main
 if __name__ == "__main__":
+    port = 31339
+    ip = -1
+    
+    #args handling
     if len(sys.argv) == 3:
-        tn = connect(sys.argv[1], sys.argv[2])
+        port = sys.argv[2]
+        ip = sys.argv[1]
+        tn = connect(ip, port)
     elif len(sys.argv) == 2:
-        tn = connect(sys.argv[1])
+        ip = sys.argv[1]
+        tn = connect(ip)
     else:
         print("Incorrect script args")
         print("usage: python cTivoTelnetControl.py ip <port=31339>")
@@ -45,10 +53,26 @@ if __name__ == "__main__":
 
     #go in key listening loop, send command as needed
     while True:
-        c = getch.getch().decode("ascii")
+        try:
+            c = getch.getch().decode("ascii")
+        except UnicodeDecodeError:
+            print("Can't decode that key into binary, not usable")
+            #get one more key because arrows may be two seperate getch calls
+            getch.getch().decode("ascii")
+            continue
+
         if c not in keydict:
             print(c + " not in keydict, exiting...")
             break
+
         telnet_cmd = keydict[c] + os.linesep
         print ("recv'd \"" + c + "\" sending:" + telnet_cmd + " to sys.argv[1]")
-        tn.write(str.encode(telnet_cmd))
+
+        try:
+            tn.write(str.encode(telnet_cmd))
+        except socket.error:
+            print ("lost connection, reconnecting")
+            tn = connect(ip, port) 
+            tn.write(str.encode(telnet_cmd))
+
+    tn.close()
